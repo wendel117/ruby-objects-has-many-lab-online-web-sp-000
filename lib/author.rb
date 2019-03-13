d59bd1e29b250bbca338cb22a8aef72be03785b1
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-13 16:31:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 
 attr_accessor :name
-
+@@post_count = 0
 
 def initialize(name)
   @name = name
@@ -11,6 +11,7 @@ end
 def add_post(post)
   @posts << post
   post.author = self
+  @@post_count += 1
 end
 
 def posts
